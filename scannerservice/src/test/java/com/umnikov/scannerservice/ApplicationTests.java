6f674b92be70de61ea431864c6289a84ea15d5f1
romaum <clevernovel@gmail.com>
2019-06-21 21:29:45 +0300

    add scannerlib

--- scannerservice/src/test/java/com/umnikov/scannerservice/ApplicationTests.java
+++ scannerservice/src/test/java/com/umnikov/scannerservice/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.SpringBootRestService;
+package com.umnikov.scannerservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootRestServiceApplicationTests {
+public class ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Test
+  public void contextLoads() {
+  }
 
 }
