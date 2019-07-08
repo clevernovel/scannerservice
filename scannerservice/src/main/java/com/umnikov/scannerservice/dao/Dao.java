ad20b1f37d9df64cfa8d478b1eac0871f1c28fc9
romaum <clevernovel@gmail.com>
2019-07-08 12:08:56 +0300

    new list ids

--- scannerservice/src/main/java/com/umnikov/scannerservice/dao/Dao.java
+++ scannerservice/src/main/java/com/umnikov/scannerservice/dao/Dao.java
@@ -1,16 +1,11 @@
 package com.umnikov.scannerservice.dao;
 
-import com.umnikov.scannerlib.dto.UserDto;
-import com.umnikov.scannerservice.entity.User;
-import com.umnikov.scannerservice.exceptions.NotFoundException;
-import com.umnikov.scannerservice.services.ScannerService;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Dao<T> {
@@ -27,15 +22,8 @@ public abstract class Dao<T> {
     return session().get(clazz, id);
   }
 
-  public List<T> byIds(final int ids) {
-    List list = new ArrayList();
-    UserDto userDto = new UserDto();
-    UserDao userDao = new UserDao();
-    ScannerService scannerService = new ScannerService(userDao);
-    for (int i = 0; i < ids; i++) {
-      scannerService.getUserById(i);
-      list.add(userDao.byId((long) i));
-    }
+  public List<T> byIds(final Long ids) {
+    List<T> list = session().byMultipleIds(clazz).multiLoad(ids);
     return list;
   }
 
